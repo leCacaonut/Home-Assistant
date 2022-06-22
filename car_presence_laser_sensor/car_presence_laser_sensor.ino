100f21969df65557e7296e1c6e0b8687d7bdda8d
leCacaonut <47879848+leCacaonut@users.noreply.github.com>
2022-06-22 23:04:17 +1000

    fix communication

--- car_presence_laser_sensor/car_presence_laser_sensor.ino
+++ car_presence_laser_sensor/car_presence_laser_sensor.ino
@@ -33,100 +33,100 @@ boolean previousState = false;
 /* ------------------------------------------- */
 // Connecting to Wifi
 void wifiReconnect() {
-  WiFi.disconnect();
-  WiFi.begin(ssid, password);
-  delay(100);
-  if (WiFi.status() == WL_CONNECTED) {
-    Serial.println();
-    Serial.println("Wifi connected");
-    Serial.print("IP Address: ");
-    Serial.println(WiFi.localIP());
-  }
+    WiFi.disconnect();
+    WiFi.begin(ssid, password);
+    delay(100);
+    if (WiFi.status() == WL_CONNECTED) {
+        Serial.println();
+        Serial.println("Wifi connected");
+        Serial.print("IP Address: ");
+        Serial.println(WiFi.localIP());
+    }
 }
 
 // Connecting to MQTT Server
 boolean mqttReconnect() {
-  if (client.connect(hostname, "mqttuser", "mqttuser")) {
-    Serial.println("Connected to mqtt");
-    // client.subscribe(sub_topic1);
-    client.publish(connected_topic, "Connected");
-    //        client.publish(sub_topic1, dtostrf(distanceClose, 4, 1, temp));
-  }
-  return client.connected();
+    if (client.connect(hostname, "mqttuser", "mqttuser")) {
+        Serial.println("Connected to mqtt");
+        // client.subscribe(sub_topic1);
+        client.publish(connected_topic, "Connected");
+        //        client.publish(sub_topic1, dtostrf(distanceClose, 4, 1, temp));
+    }
+    return client.connected();
 }
 
 // When a message is received
 void callback(char* topic, byte* payload, unsigned int length) {
-  char copy[length + 1];
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-  int i = 0;
-  for (i = 0; i < length; i++) {
-    //        Serial.print((char)payload[i]);
-    copy[i] = (char)payload[i];
-  }
-  copy[++i] = '\0';  // End the c string
-  Serial.println(copy);
+    char copy[length + 1];
+    Serial.print("Message arrived [");
+    Serial.print(topic);
+    Serial.print("] ");
+    int i = 0;
+    for (i = 0; i < length; i++) {
+        //        Serial.print((char)payload[i]);
+        copy[i] = (char)payload[i];
+    }
+    copy[++i] = '\0';  // End the c string
+    Serial.println(copy);
 
-  // if (strcmp(topic, sub_topic1) == 0) sendMessage = true;
+    // if (strcmp(topic, sub_topic1) == 0) sendMessage = true;
 }
 
 void setup() {
-  Serial.begin(115200);
-  // Wifi setup
-  WiFi.mode(WIFI_STA);
-  WiFi.setHostname(hostname);
-  // Mqtt setup
-  client.setServer(server, port);
-  client.setCallback(callback);
-  // Sensor
-  if (!laserSensor.begin()) {
-    Serial.println("Failed to initialise sensor");
-  }
+    Serial.begin(115200);
+    // Wifi setup
+    WiFi.mode(WIFI_STA);
+    WiFi.setHostname(hostname);
+    // Mqtt setup
+    client.setServer(server, port);
+    client.setCallback(callback);
+    // Sensor
+    if (!laserSensor.begin()) {
+        Serial.println("Failed to initialise sensor");
+    }
 }
 
 void loop() {
-  unsigned long currentMillis = millis();
-  // Enter sensor reading code here
-  VL53L0X_RangingMeasurementData_t measure;
-  laserSensor.rangingTest(&measure, false);
-  if (measure.RangeStatus != 4) {  // phase failures have incorrect data
-    // Serial.print("Distance (mm): "); Serial.println(measure.RangeMilliMeter);
-    objectDetected = true;
-  } else {
-    // Serial.println(" out of range ");
-    objectDetected = false;
-  }
-
-  // End code
-  if ((WiFi.status() != WL_CONNECTED) && (currentMillis - wifiLastMillis > reconnectInterval)) {
-    Serial.print("Connecting to wifi...");
-    wifiReconnect();
-    wifiLastMillis = currentMillis;
-  } else if (WiFi.status() == WL_CONNECTED) {
-    if (!client.connected()) {
-      currentMillis = millis();
-      if (currentMillis - mqttLastMillis > reconnectInterval) {
-        Serial.print("Connecting to mqtt...");
-        mqttReconnect();
-      }
+    unsigned long currentMillis = millis();
+    // Enter sensor reading code here
+    VL53L0X_RangingMeasurementData_t measure;
+    laserSensor.rangingTest(&measure, false);
+    if (measure.RangeStatus != 4 && measure.RangeMilliMeter < 1200) {  // phase failures have incorrect data
+         Serial.print("Distance (mm): "); Serial.println(measure.RangeMilliMeter);
+        objectDetected = true;
     } else {
-
-      // Enter custom code here
-      currentMillis = millis();
-      if (currentMillis - sendLastMillis > sendInterval) {
-        if (previousState != objectDetected) {
-          Serial.println("Sent mqtt");
-          if (objectDetected) client.publish(pub_topic1, "true");
-          else client.publish(pub_topic1, "false");
-          previousState = objectDetected;
-
-          sendLastMillis = currentMillis;
-        }
-      }
-      // End code
-      client.loop();
+        // Serial.println(" out of range ");
+        objectDetected = false;
     }
-  }
+
+    // End code
+    if ((WiFi.status() != WL_CONNECTED) && (currentMillis - wifiLastMillis > reconnectInterval)) {
+        Serial.print("Connecting to wifi...");
+        wifiReconnect();
+        wifiLastMillis = currentMillis;
+    } else if (WiFi.status() == WL_CONNECTED) {
+        if (!client.connected()) {
+            currentMillis = millis();
+            if (currentMillis - mqttLastMillis > reconnectInterval) {
+                Serial.print("Connecting to mqtt...");
+                mqttReconnect();
+            }
+        } else {
+
+            // Enter custom code here
+            currentMillis = millis();
+            if (currentMillis - sendLastMillis > sendInterval) {
+                if (previousState != objectDetected) {
+                    Serial.println("Sent mqtt");
+                    if (objectDetected) client.publish(pub_topic1, "1");
+                    else client.publish(pub_topic1, "0");
+                    previousState = objectDetected;
+
+                    sendLastMillis = currentMillis;
+                }
+            }
+            // End code
+            client.loop();
+        }
+    }
 }
